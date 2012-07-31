b3dd8e9c0366635140140607d2f5e81a22793feb
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2012-07-31 18:06:57 +0300

    Initial DemoApp with posgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demopsql::Application.config.secret_token = 'b53cdbf6ea63f67a0557836aaa24e9da259ca029083f5dcfbc5f9b8c555951caf4210f322a0f60564f20f44513a8193e5bd6dd989cc3ad4c7ac7f78b16e1d777'
